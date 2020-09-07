1a6d9848da5f1845ac2979fa59d4b21814b2684d
suingam <69798748+suingam@users.noreply.github.com>
2020-09-07 17:55:29 +0100

    Add files via upload

--- Rcodeassignment1.R
+++ Rcodeassignment1.R
@@ -23,38 +23,20 @@ if(file.exists("household_power_consumption.txt")) {
 
 t <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings = "?", colClasses = c('character','character','numeric','numeric','numeric','numeric','numeric','numeric','numeric'))
 
-## Format date to Type Date
 t$Date <- as.Date(t$Date, "%d/%m/%Y")
-
-## Filter data set from Feb. 1, 2007 to Feb. 2, 2007
 t <- subset(t,Date >= as.Date("2007-2-1") & Date <= as.Date("2007-2-2"))
-
-## Remove incomplete observation
 t <- t[complete.cases(t),]
-
-## Combine Date and Time column
 dateTime <- paste(t$Date, t$Time)
-
-## Name the vector
 dateTime <- setNames(dateTime, "DateTime")
-
-## Remove Date and Time column
 t <- t[ ,!(names(t) %in% c("Date","Time"))]
-
-## Add DateTime column
 t <- cbind(dateTime, t)
-
-## Format dateTime Column
 t$dateTime <- as.POSIXct(dateTime)
 
-
 hist(t$Global_active_power, main="Global Active Power", xlab = "Global Active Power (kilowatts)", col="red")
-
 dev.copy(png,"plot1.png", width=480, height=480)
 dev.off()
 
 plot(t$Global_active_power~t$dateTime, type="l", ylab="Global Active Power (kilowatts)", xlab="")
-
 dev.copy(png,"plot2.png", width=480, height=480)
 dev.off()
 
